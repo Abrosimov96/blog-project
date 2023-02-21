405c23910461b82677d0ce6388e9da8c7d401b8f
Abrosimov96 <Abrosimov0423@gmail.com>
2023-02-21 19:46:33 +0200

    fix modal styles

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark'
-  }
+  LIGHT = 'app_light_theme',
+  DARK = 'app_dark_theme'
+}
 
 export interface ThemeContextProps {
-    theme?: Theme;
-    setTheme?: (theme: Theme) => void
-  }
+  theme?: Theme;
+  setTheme?: (theme: Theme) => void
+}
 
 export const ThemeContext = React.createContext<ThemeContextProps>({})
 
